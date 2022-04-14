5ea025b96a9fdfd01c5f76c363d3bf9eefc5eb3a
Carl Myers <cmyers@cmyers.org>
2022-04-14 01:47:41 -0700

    Calculates pct time in target now

--- README.md
+++ README.md
@@ -14,7 +14,9 @@ $ ./analyze.py
 
 # TODO
 * clean up graph, make it prettier
+* tag data points - green if surrounding 24hrs is >80% in target?
 * produce weekly graphs over a time interval longer than 1 week (i.e. for a month, produce 4 week-long graphs)
 * produce daily aggregate average graph
 * produce weekly aggregate average graph
 * produce change graphs - weekly, daily, or monthly, Sun-Saturday with each time interval in a gradient color to see most trends over time
+* calculate average glucose over data - use convolve and window?  https://stackoverflow.com/questions/15959819/time-series-averaging-in-numpy-python
--- analyze.py
+++ analyze.py
@@ -4,6 +4,7 @@ import csv
 import datetime
 import dateutil
 import logging
+import memoization
 import os
 import pprint
 import sys
@@ -84,6 +85,10 @@ def date_to_output(datetime):
 
 
 def main():
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    logging.getLogger('matplotlib').setLevel(logging.INFO)
+
     logging.info(f"File to analyze: {sys.argv[1]}")
 
     time_data = []
@@ -119,6 +124,222 @@ def main():
     sys.exit(0)
 
 
+@memoization.cached
+def calculate_time_glucose_transitions(time_a, glucose_a, target_glucose, time_b, glucose_b):
+    """
+    Calculates the approximate time between two points the glucose level hits a
+    certain value via linear extrapolation.
+
+    Precondition:  time_a must be before time_b, target_glucose must be between
+    glucose a and glucose_b, or I shall not be held responsible for the
+    results! =)
+
+    # ### test code TODO: test this, but it works!
+    time_a = datetime.datetime.now()
+    time_b = time_a + datetime.timedelta(seconds=100)
+
+    glucose_a = 100
+    glucose_b = 200
+
+    print(f"Result should be {time_a + datetime.timedelta(seconds=20)}: " + str(calculate_time_glucose_transitions(time_a, glucose_a, 120, time_b, glucose_b)))
+
+    print(f"Result should be {time_a + datetime.timedelta(seconds=45)}: " + str(calculate_time_glucose_transitions(time_a, glucose_a, 145, time_b, glucose_b)))
+
+    print(f"Result should be {time_a + datetime.timedelta(seconds=15)}: " + str(calculate_time_glucose_transitions(time_a, glucose_b, 185, time_b, glucose_a)))
+    # ### end test code
+    """
+
+    glucose_range = glucose_b - glucose_a
+    time_delta_to_transition = ((target_glucose - glucose_a) / (glucose_b - glucose_a)) * (time_b - time_a)
+    return time_a + time_delta_to_transition
+
+
+@memoization.cached
+def calculate_glucose_between_two_times(time_a, glucose_a, target_time, time_b, glucose_b):
+    """
+    Calculates the approximate glucose at a given time between two data
+    points via linear extrapolation.
+
+    Precondition:  time_a must be before target_time, time_b must be after
+    target_time, or I shall not be held responsible for the results! =)
+
+    TODO: write these as tests?  It works!
+    # #### TEST CODE
+    time_a = datetime.datetime.now()
+    time_b = time_a + datetime.timedelta(days=1)
+    glucose_a = 100
+    glucose_b = 200
+
+    target_one = time_a + datetime.timedelta(hours=4)
+    glucose_one = ((200.0-100) * (1.0/6))+100
+
+    target_two = time_a + datetime.timedelta(hours=12)
+    glucose_two = 150.0
+
+    backwards_target_three = time_a + datetime.timedelta(hours=4)
+    backwards_glucose_three = ((100.0-200) * (1.0/6))+200
+
+    result = calculate_glucose_between_two_times(time_a, glucose_a, target_one, time_b, glucose_b)
+    print(f"Expected result {glucose_one}, got {result}")
+
+    result = calculate_glucose_between_two_times(time_a, glucose_a, target_two, time_b, glucose_b)
+    print(f"Expected result {glucose_two}, got {result}")
+
+    result = calculate_glucose_between_two_times(time_a, glucose_b, backwards_target_three, time_b, glucose_a)
+    print(f"Expected result {backwards_glucose_three}, got {result}")
+    # #### END TEST CODE
+
+    """
+    timedelta_before = target_time - time_a
+    timedelta_after = time_b - target_time
+
+    ratio_to_apply = timedelta_before / (timedelta_before + timedelta_after)
+    return glucose_a + ((glucose_b - glucose_a) * ratio_to_apply)
+
+
+def get_tz_state(tz_min, tz_max, glucose):
+    """
+    Returns -1 if below target zone, +1 if above, and 0 if inside target zone.
+    """
+    if glucose < tz_min:
+        return -1
+    if glucose > tz_max:
+        return 1
+    return 0
+
+
+# TODO: the arguments to this are massive, is this a bad idea to memoize?
+@memoization.cached
+def calculate_time_in_target(tz_min, tz_max, time_data, glucose, start_date=None, end_date=None):
+    """
+    Calculates the ratio of time between start_date and end_date that was in
+    the target zone. (i.e., values 0 to 1)
+
+    If either start or end is omitted, uses the start or end of data, respectively.
+
+    If there is no data for the given time, returns 0.
+
+    The calculation is performed thusly: when point B is on the opposite side
+    of the target zone immediately after point A, the transition time is
+    calculated by calculating the slope of the line connecting those points
+    then calculating where that line crosses the border of the zone.
+
+    # ### TODO TEST CODE
+    time_data, glucose = graphify_data(data)
+
+    logging.debug("Calculating TZ data")
+
+    start_date = datetime.datetime(year=2022, month=4, day=3)
+    end_date = start_date+datetime.timedelta(days=7)
+    time_in_tz = calculate_time_in_target(config['target_min'], config['target_max'], time_data, glucose, start_date, end_date)
+    logging.debug(f"Time in target zone from {start_date} to {end_date} is {time_in_tz*100:.2f}")
+    # ### END TEST CODE
+
+    """
+    # Find the start point and end point
+    if start_date is None:
+        start_date = time_data[0]
+    if end_date is None:
+        end_date = time_data[-1]
+
+    # if there is no data, return 0
+    if start_date > time_data[-1]:
+        return 0
+    if end_date < time_data[0]:
+        return 0
+
+    # if start date is before first data point, or after last data point, truncate range to our data.
+    if start_date < time_data[0]:
+        start_date = time_data[0]
+    if end_date > time_data[-1]:
+        end_date = time_data[-1]
+
+    # we might be given a start/end time which falls between two data points.
+    # If that happens, we need to calculate the state at that time using our
+    # avg method.
+    time_deltas_in_zone = []
+    tz_state = None
+    new_tz_state = None
+    last_transition_date = start_date
+    for i in range(len(time_data)):
+        if start_date == time_data[i]:
+            tz_state = get_tz_state(tz_min, tz_max, glucose[i])
+            continue
+        if start_date < time_data[i] and start_date > time_data[i - 1]:
+            start_glucose = calculate_glucose_between_two_times(time_data[i - 1], glucose[i - 1], start_date, time_data[i], glucose[i])
+            tz_state = get_tz_state(tz_min, tz_max, start_glucose)
+            continue
+        if tz_state is None:
+            # haven't found start yet
+            continue
+        # we are now in the range of interest.  See if we are at the end?
+        if end_date == time_data[i]:
+            # if we are at the end, all that matters is that if we are in our
+            # TZ, we need to count this stretch as a timedelta in the tz.
+            if tz_state == 0:
+                time_deltas_in_zone.append(end_date - last_transition_date)
+                break
+        if time_data[i] > end_date:
+            if tz_state == 0:
+                # we were in target zone, make sure we still are
+                # we went past the end of the range, so let's calculate what the glucose was exactly at the end.
+                end_glucose = calculate_glucose_between_two_times(time_data[i - 1], glucose[i - 1], end_date, time_data[i], glucose[i])
+                new_tz_state = get_tz_state(tz_min, tz_max, end_glucose)
+                if new_tz_state == 0:
+                    # in the zone until the end
+                    time_deltas_in_zone.append(end_date - last_transition_date)
+                    break
+            # else, we are not in tz, who cares if we are just starting being in the zone, it's right at the end.  we can probably drop that datapoint.
+            break
+
+        # ok, not at the end yet, detect a mid-range transition
+        new_tz_state = get_tz_state(tz_min, tz_max, glucose[i])
+        if new_tz_state != tz_state:
+            # we have transitioned.  3 states, 6 possible transitions:
+            # 1 to 0, 1 to -1, 0 to 1, 0 to -1, -1 to 0, -1 to 1.
+            # BUG: for now, we will ignore transitions from -1 to 1 or 1 to -1
+            # what we COULD do is linear extrapolate when you entered and left
+            # the zone but frankly if you have holes in your data that big, big
+            # enough to make that jump, you probably aren't going to get useful
+            # data anyways.
+            pair = (tz_state, new_tz_state)
+            if pair == (-1, 1) or pair == (1, -1):
+                # still outside tz, nothing to do
+                continue
+            if pair == (0, 1):
+                # was in tz, left tz on top
+                time_left_tz = calculate_time_glucose_transitions(time_data[i - 1], glucose[i - 1], tz_max, time_data[i], glucose[i])
+                time_deltas_in_zone.append(time_left_tz - last_transition_date)
+                last_transition_date = time_left_tz
+                tz_state = new_tz_state
+                continue
+            if pair == (0, -1):
+                # was in tz, left tz on bottom
+                time_left_tz = calculate_time_glucose_transitions(time_data[i - 1], glucose[i - 1], tz_min, time_data[i], glucose[i])
+                time_deltas_in_zone.append(time_left_tz - last_transition_date)
+                last_transition_date = time_left_tz
+                tz_state = new_tz_state
+                continue
+            if pair == (1, 0):
+                # entered tz on top
+                time_entered_tz = calculate_time_glucose_transitions(time_data[i - 1], glucose[i - 1], tz_max, time_data[i], glucose[i])
+                last_transition_date = time_entered_tz
+                tz_state = new_tz_state
+                continue
+            if pair == (-1, 0):
+                # entered tz on bottom
+                time_entered_tz = calculate_time_glucose_transitions(time_data[i - 1], glucose[i - 1], tz_min, time_data[i], glucose[i])
+                last_transition_date = time_entered_tz
+                tz_state = new_tz_state
+                continue
+    # ok, we have collected all the time deltas in the TZ.  Ratio in tz is now sum of all deltas over end - start.
+    total_time_in_tz = sum(time_deltas_in_zone, datetime.timedelta())
+    total_time_range = end_date - start_date
+    ratio = total_time_in_tz / total_time_range
+    logging.debug(f"Found {len(time_deltas_in_zone)} times in tz totalling {total_time_in_tz} out of {total_time_range} or {ratio*100:.2f}%")
+    return ratio
+
+
 def graphify_data(data, start_date=None, end_date=None):
     logging.debug("Graphifying data set")
     if start_date:
@@ -147,12 +368,14 @@ def graphify_data(data, start_date=None, end_date=None):
             time_data.append(data[ts]['datetime'])
             glucose.append(int(data[ts]['historic_glucose']))
             continue
-        logging.debug(f"Skipping timestamp {ts} because it has no glucose data")
+        #logging.debug(f"Skipping timestamp {ts} because it has no glucose data")
     return (time_data, glucose)
 
 
 def generate_plot_from_data(output_file, title, config, time_data, glucose):
     # some calculations before we get started...
+    # time in target for entire graph
+    tz_time = calculate_time_in_target(config['target_min'], config['target_max'], time_data, glucose)
     # y_min = int(np.min(glucose) * 0.95)
     # y_max = int(np.max(glucose) * 1.05)
     # y_ticks = 20
@@ -167,7 +390,7 @@ def generate_plot_from_data(output_file, title, config, time_data, glucose):
     }):
 
         fig, ax = plt.subplots()
-        fig.suptitle(title)
+        fig.suptitle(title + f" (time in target: {tz_time*100:.1f}%)")
         ax.set_xlabel("Date")
         ax.set_ylabel("Blood Glucose Level (mg/dL)")
         ax.set_yticks(np.arange(30, 500, 20))
